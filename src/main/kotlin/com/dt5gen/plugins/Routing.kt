17fe08a31db2c67f94efc7b39ad58686205b9521
Alx_Shilov <deeppowercrew@gmail.com>
2024-10-13 21:21:23 +0300

    add Routing.kt and config requests

--- src/main/kotlin/com/dt5gen/plugins/Routing.kt
+++ src/main/kotlin/com/dt5gen/plugins/Routing.kt
@@ -1,13 +1,27 @@
 package com.dt5gen.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.request.uri
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         get("/"){
+
+            println("URI: ${call.request.uri}")  // '/'
+            println("Headers: ${call.request.headers.names()}")
+            println("User-Agent: ${call.request.headers["User-Agent"]}")
+            println("Accept: ${call.request.headers["Accept"]}")
+            println("Query-params: ${call.request.queryParameters.names()}")
+            println("Name: ${call.request.queryParameters["name"]}")
+            println("Email: ${call.request.queryParameters["email"]}")
+
             call.respond("Hello fucking World!")
+        }
+
+        get("/notes/{page}"){
+            call.respondText("You are on page: ${call.parameters["page"]}")
         }
     }
 }
