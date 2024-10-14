6d3326c4ca13c58103f63b412c33f592f2e5899d
Alx_Shilov <deeppowercrew@gmail.com>
2024-10-14 03:19:59 +0300

    test downloading files

--- src/main/kotlin/com/dt5gen/plugins/Routing.kt
+++ src/main/kotlin/com/dt5gen/plugins/Routing.kt
@@ -1,11 +1,16 @@
 package com.dt5gen.plugins
 
+
+import io.ktor.http.ContentDisposition
+import io.ktor.http.HttpHeaders
+
 import io.ktor.server.application.*
 import io.ktor.server.request.receive
 import io.ktor.server.request.uri
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
@@ -26,7 +31,33 @@ fun Application.configureRouting() {
             call.respondText("You are on page: ${call.parameters["page"]}")
         }
 
-        post("/login"){
+        get("/download/1") {
+            val file = File("/Users/marin/IdeaProjects/ktor-app/files/logo BBTTR2.png")
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment.withParameter(
+                    ContentDisposition.Parameters.FileName, "downloadableImage.png"
+
+                ).toString()
+            )
+            call.respondFile(file)
+
+
+        }
+
+        get("/download/2") {
+            val file2 = File("/Users/marin/IdeaProjects/ktor-app/files/log_nin_car.webp")
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Inline.withParameter(
+                    ContentDisposition.Parameters.FileName, "downloadableImage2.webp"
+
+                ).toString()
+            )
+            call.respondFile(file2)
+        }
+
+        post("/login") {
             val userInfo = call.receive<UserInfo>()
             println(userInfo)
             call.respondText("You are logged in")
