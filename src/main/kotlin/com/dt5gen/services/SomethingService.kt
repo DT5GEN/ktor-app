2b587972b7903f4f2ab837ac5bd584e7cd87a677
Alx_Shilov <deeppowercrew@gmail.com>
2024-10-15 13:01:01 +0300

    init creation new table on DB

--- src/main/kotlin/com/dt5gen/services/SomethingService.kt
+++ src/main/kotlin/com/dt5gen/services/SomethingService.kt
@@ -9,33 +9,50 @@ import org.ktorm.dsl.select
 
 class SomethingService(private val database: Database) {
 
-    // Функция для получения всех пользователей из таблицы
-    fun fetchAllUsers(): List<UserModel> {
-        return database
-            .from(com.dt5gen.data.models.User)  // Обращаемся к таблице User
-            .select()
-            .map { row ->
-                UserModel(  // Создаём экземпляр UserModel для каждой строки
-                    email = row[com.dt5gen.data.models.User.email] ?: "",
-                    name = row[com.dt5gen.data.models.User.name] ?: "",
-                    hashPassword = row[com.dt5gen.data.models.User.hashPassword] ?: ""
-                )
-            }
+    // Function for create new table
+
+    fun createUsersTable() {
+        database.useConnection { connection ->
+            val statement = connection.createStatement()
+            statement.executeUpdate(
+                """
+                CREATE TABLE IF NOT EXISTS users (
+                    id SERIAL PRIMARY KEY,
+                    username VARCHAR(100) NOT NULL,
+                    password VARCHAR(500) NOT NULL
+                );
+            """.trimIndent()
+            )
+            statement.close()
+        }
+
+        // Функция для получения всех пользователей из таблицы
+        fun fetchAllUsers(): List<UserModel> {
+            return database
+                .from(com.dt5gen.data.models.User)  // Обращаемся к таблице User
+                .select()
+                .map { row ->
+                    UserModel(  // Создаём экземпляр UserModel для каждой строки
+                        email = row[com.dt5gen.data.models.User.email] ?: "",
+                        name = row[com.dt5gen.data.models.User.name] ?: "",
+                        hashPassword = row[com.dt5gen.data.models.User.hashPassword] ?: ""
+                    )
+                }
+        }
+
+        // Функция для получения всех notes из таблицы
+        fun fetchAllNotes(): List<NotesModel> {
+            return database
+                .from(com.dt5gen.data.models.NotesEntity)  // Обращаемся к таблице User
+                .select()
+                .map { row ->
+                    NotesModel(
+                        id = row[com.dt5gen.data.models.NotesEntity.id] ?: "",
+                        note = row[com.dt5gen.data.models.NotesEntity.note] ?: ""
+                    )
+                }
+        }
+
     }
-
-    // Функция для получения всех notes из таблицы
-    fun fetchAllNotes(): List<NotesModel> {
-        return database
-            .from(com.dt5gen.data.models.NotesEntity)  // Обращаемся к таблице User
-            .select()
-            .map { row ->
-                NotesModel(
-                    id = row[com.dt5gen.data.models.NotesEntity.id] ?: "",
-                    note = row[com.dt5gen.data.models.NotesEntity.note] ?: ""
-                )
-            }
-    }
-
-
 }
 
