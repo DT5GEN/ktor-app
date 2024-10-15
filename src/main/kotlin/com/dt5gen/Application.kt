104b8977d4ed3f8966eaa088a234ac4a1f06d31d
Alx_Shilov <deeppowercrew@gmail.com>
2024-10-15 15:26:04 +0300

    prepare validation

--- src/main/kotlin/com/dt5gen/Application.kt
+++ src/main/kotlin/com/dt5gen/Application.kt
@@ -1,20 +1,13 @@
 package com.dt5gen
 
 
-import com.dt5gen.data.models.NotesEntity
-import com.dt5gen.data.models.NotesEntity.note
 import com.dt5gen.db.DatabaseConnection
 import com.dt5gen.plugins.*
-import com.dt5gen.services.SomethingService
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import org.ktorm.database.Database
-import org.ktorm.dsl.from
-import org.ktorm.dsl.map
-import org.ktorm.dsl.select
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
